300f6eb187446e623111a9987593971ebe8c2fed
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-09-22 15:30:05 +0300

    remove old libjpeg

--- setup-travis.sh
+++ setup-travis.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
 
 wget http://ubuntu.ipacct.com/ubuntu//pool/main/libj/libjpeg8/libjpeg8_8b-1_i386.deb
 wget http://se.archive.ubuntu.com/ubuntu/pool/main/libj/libjpeg8/libjpeg8-dev_8b-1_i386.deb
+
+sudo apt-get remove -y libjpeg62-dev libjpeg62
 
 sudo dpkg -i libjpeg8_8b-1_i386.deb
 sudo dpkg -i libjpeg8-dev_8b-1_i386.deb
