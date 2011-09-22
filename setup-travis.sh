bf2037e5dc8fe3650bb133f408c302bec9e35669
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-09-22 15:27:21 +0300

    try manyally installing libjpg

--- setup-travis.sh
+++ setup-travis.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-sudo apt-get install -y libcairo2 libcairo2-dev
+wget http://ubuntu.ipacct.com/ubuntu//pool/main/libj/libjpeg8/libjpeg8_8b-1_i386.deb
+wget http://se.archive.ubuntu.com/ubuntu/pool/main/libj/libjpeg8/libjpeg8-dev_8b-1_i386.deb
+
+dpkg -i libjpeg8_8b-1_i386.deb
+dpkg -i libjpeg8-dev_8b-1_i386.deb
+
+sudo apt-get install -y libcairo2 libcairo2-dev libc-dev
+
 
 npm install --dev
 
