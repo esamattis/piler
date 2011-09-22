ebb93e1ea064d7ef1ffe1bf06496e1522d74f8f1
Esa-Matti Suuronen <esa-matti@suuronen.org>
2011-09-22 15:28:35 +0300

    add some superuser permissions

--- setup-travis.sh
+++ setup-travis.sh
@@ -3,8 +3,8 @@
 wget http://ubuntu.ipacct.com/ubuntu//pool/main/libj/libjpeg8/libjpeg8_8b-1_i386.deb
 wget http://se.archive.ubuntu.com/ubuntu/pool/main/libj/libjpeg8/libjpeg8-dev_8b-1_i386.deb
 
-dpkg -i libjpeg8_8b-1_i386.deb
-dpkg -i libjpeg8-dev_8b-1_i386.deb
+sudo dpkg -i libjpeg8_8b-1_i386.deb
+sudo dpkg -i libjpeg8-dev_8b-1_i386.deb
 
 sudo apt-get install -y libcairo2 libcairo2-dev libc-dev
 
